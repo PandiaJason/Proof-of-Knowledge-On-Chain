49be789798d81fbfd6bcf10cd765a12bbdacc810
PandiaJason <100123063+PandiaJason@users.noreply.github.com>
2022-12-09 11:57:26 +0530

    Update ISetup.sol

--- ISetup.sol
+++ ISetup.sol
@@ -2,17 +2,17 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.0;
 
-import "./IPIS1.sol";
+import "./ERC721URIStorage.sol";
 import "@openzeppelin/contracts/utils/Counters.sol";
 
-contract Immutable is ERC721URIStorage {
-    using Counters for Counters.Counter;
+contract NTT is ERC721URIStorage {
+  using Counters for Counters.Counter;
     Counters.Counter private _tokenIds;
     // address[]  public nntHolder;
     address public immutable nntHolder;
 
 
-    constructor(address  _nntHolder) ERC721("non-transferable token", "NTT") {
+    constructor(address  _nntHolder) ERC721("Non-Transferable-Token", "NTT") {
         nntHolder = _nntHolder;
     }
 
