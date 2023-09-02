714b06e68c23035af59d2c22be3155aaf0691ef5
JSN <100123063+PandiaJason@users.noreply.github.com>
2023-09-02 19:23:23 +0530

    Update NTT.sol

--- contracts/NTT.sol
+++ contracts/NTT.sol
@@ -2,15 +2,19 @@
 pragma solidity ^0.8.0;
 
 import "https://github.com/PandiaJason/Non-Transferable-Non-Fungible-Tokens/blob/main/contracts/ERC721URIStorage.sol";
+import "@openzeppelin/contracts/utils/math/SafeMath.sol";
+import "@openzeppelin/contracts/security/ReentrancyGuard.sol";
 
 /**
 * @dev Implementation of https://github.com/PandiaJason/Non-Transferable-Non-Fungible-Tokens 
 * Non_Transferable Token with MultiSig Scheme
-* {Modified Extention of ERC721}.
+* {Modified Extension of ERC721}.
 */
 
-contract NTT is ERC721URIStorage {
-    // immutable nttRECIPIENT address to keep NTT
+contract NTT is ERC721URIStorage, ReentrancyGuard {
+    using SafeMath for uint256; // Import SafeMath
+
+    // Immutable nttRECIPIENT address to keep NTT
     address public immutable nttRECIPIENT;
 
     // Mapping of tokenID => uri 
@@ -24,9 +28,7 @@ contract NTT is ERC721URIStorage {
     // Mapping of tokenID => tokenTHRESHOLD 
     mapping(uint256 => tokenTHRESHOLD) public tokensTDATA;
 
-
     constructor(address  _nttRECIPIENT) ERC721("Non-Transferable-Token", "NTT") {
-    // constructor(address  _nntHolder, string memory name_, string memory symbol_) ERC721(name_, symbol_) {
         nttRECIPIENT = _nttRECIPIENT;
     }
     
@@ -36,31 +38,30 @@ contract NTT is ERC721URIStorage {
     * return uriDATA {string}
     */
     function uriDATA(uint256 tokenID, string memory uri)
-        public returns (string memory )
+        public nonReentrant returns (string memory)
     {   
         require(registry[msg.sender] == true, "Not an authoritarian" );
-        require(bytes(tokensINFO[tokenID]).length == 0, "URI already exits");
-        tokensINFO[tokenID]= uri;
+        require(bytes(tokensINFO[tokenID]).length == 0, "URI already exists");
+        tokensINFO[tokenID] = uri;
         return tokensINFO[tokenID];
     }
 
     /**
     * @dev Implementation of function mintNTT
     * verifies msg.sender, uri length, tokenTVAL and 
-    * trigers mint, setTokenURI 
+    * triggers mint, setTokenURI 
     * return tokenID {uint256}
     */
     function mintNTT(uint256 tokenID)
-        public returns (uint256)
+        public nonReentrant returns (uint256)
     {
-        require(bytes(tokensINFO[tokenID]).length != 0, "URI doesn't exits");
-        require( tokensTDATA[tokenID].tokenTVAL >= 2, "Doesn't  met the threshold");
+        require(bytes(tokensINFO[tokenID]).length != 0, "URI doesn't exist");
+        require(tokensTDATA[tokenID].tokenTVAL >= 2, "Doesn't meet the threshold");
         _mint(nttRECIPIENT, tokenID);
         _setTokenURI(tokenID, tokensINFO[tokenID]);
 
         return tokenID;
     }
-
 
     /**
     * @dev Implementation of function multiSIG
@@ -68,18 +69,18 @@ contract NTT is ERC721URIStorage {
     * appends tokenTVAL by 1, & signedAUTH
     */
     function multiSIG(uint256  _tokenID)
-    public  {
-        require(bytes(tokensINFO[_tokenID]).length != 0, "URI doesn't exits");
+    public nonReentrant
+    {
+        require(bytes(tokensINFO[_tokenID]).length != 0, "URI doesn't exist");
         require(registry[msg.sender] == true, "Not an authoritarian" );
         require( isAuthAlreadySigned(_tokenID, msg.sender ) == false, "AuthAlreadySigned");
-        tokensTDATA[_tokenID].tokenTVAL += 1;
+        tokensTDATA[_tokenID].tokenTVAL = tokensTDATA[_tokenID].tokenTVAL.add(1);
         tokensTDATA[_tokenID].signedAUTH.push(msg.sender);
-            
     }
 
     /**
     * @dev Implementation of function isAuthAlreadySigned
-    * struct tokenTHRESHOLD retrived from the storage
+    * struct tokenTHRESHOLD retrieved from the storage
     * iterate the specific tokenTDATA and check signee or not
     * bool return true or false
     */
@@ -89,8 +90,9 @@ contract NTT is ERC721URIStorage {
         tokenTHRESHOLD storage signees =  tokensTDATA[tokenID];
         for (uint i=0; i< signees.signedAUTH.length; i++){
             if (signees.signedAUTH[i] == signee){
-            return true;}
-        }return false;
+                return true;
+            }
+        }
+        return false;
     }
-
 }
